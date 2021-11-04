d6a5e1818c63257f1354124fdd8e5a036e5ac130
Hamzah Fauzy <hamzahfauzy97@gmail.com>
2021-11-04 23:00:23 +0700

    add stagging, local, and live env

--- config/database.php
+++ config/database.php
@@ -1,11 +1,22 @@
 <?php
 $server_name = $_SERVER['HTTP_HOST'];
+$local_name = ['localhost'];
+$live_name = ['z-techno.com'];
+$stagging_name = ['127.0.0.1'];
 
-if ($server_name == "localhost") {
+// local
+if (in_array($server_name,$local_name)) {
      $serverName = "localhost"; //serverName\instanceName, portNumber (default is 1433)
      $connectionInfo = array( "Database"=>"dbpajak", "UID"=>"PBBQ", "PWD"=>"0134");
-} else {
+} 
+
+if (in_array($server_name,$stagging_name)) {
      $serverName = "103.15.242.196"; //serverName\instanceName, portNumber (default is 1433)
+     $connectionInfo = array( "Database"=>"dbpajak", "UID"=>"SA", "PWD"=>"bappeda@4321");
+}
+
+if (in_array($server_name,$live)) {
+     $serverName = "127.0.0.1"; //serverName\instanceName, portNumber (default is 1433)
      $connectionInfo = array( "Database"=>"dbpajak", "UID"=>"SA", "PWD"=>"bappeda@4321");
 }
 $conn = sqlsrv_connect( $serverName, $connectionInfo);
